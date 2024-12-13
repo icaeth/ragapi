4411cde15112fde669dfba1132905f8bf8433dbc
Cristian Iriarte <iznogud@kitayori.cl>
2024-12-13 18:01:08 -0300

    pgvector

--- embeddings.py
+++ embeddings.py
@@ -15,57 +15,14 @@ embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 
 
 class RAGEmbedding:
-  connection = "postgresql+psycopg://pgvector:esvanguardia@papitas:5432"  # Uses psycopg3!
+  connection = "postgresql+psycopg://esvanguardia:papitas@pgvector:5432"  # Uses psycopg3!
   collection_name = "vectordb"
   vector_store = PGVector(    
       embeddings=embeddings,
       collection_name=collection_name,
       connection=connection,
       use_jsonb=True,
-  )
-
-  docs = [
-    Document(
-        page_content="there are cats in the pond",
-        metadata={"id": 1, "location": "pond", "topic": "animals"},
-    ),
-    Document(
-        page_content="ducks are also found in the pond",
-        metadata={"id": 2, "location": "pond", "topic": "animals"},
-    ),
-    Document(
-        page_content="fresh apples are available at the market",
-        metadata={"id": 3, "location": "market", "topic": "food"},
-    ),
-    Document(
-        page_content="the market also sells fresh oranges",
-        metadata={"id": 4, "location": "market", "topic": "food"},
-    ),
-    Document(
-        page_content="the new art exhibit is fascinating",
-        metadata={"id": 5, "location": "museum", "topic": "art"},
-    ),
-    Document(
-        page_content="a sculpture exhibit is also at the museum",
-        metadata={"id": 6, "location": "museum", "topic": "art"},
-    ),
-    Document(
-        page_content="a new coffee shop opened on Main Street",
-        metadata={"id": 7, "location": "Main Street", "topic": "food"},
-    ),
-    Document(
-        page_content="the book club meets at the library",
-        metadata={"id": 8, "location": "library", "topic": "reading"},
-    ),
-    Document(
-        page_content="the library hosts a weekly story time for kids",
-        metadata={"id": 9, "location": "library", "topic": "reading"},
-    ),
-    Document(
-        page_content="a cooking class for beginners is offered at the community center",
-        metadata={"id": 10, "location": "community center", "topic": "classes"},
-    ),
-]
+  )  
 
   def process_documents(vector_store, docs): 
     vector_store.add_documents(docs, ids=[doc.metadata["id"] for doc in docs])
