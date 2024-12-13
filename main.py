96aa3c40cbbf385dc20ef4abb52c15ccc9bfeffb
Cristian Iriarte <iznogud@kitayori.cl>
2024-12-13 14:26:10 -0300

    correcion punto en rag_system

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def query_rag(prompt: Question):
     context = "Información sobre powerBI, se entrega una imágen suministrada por el usuario como apoyo a la pregunta"
     response = rag_system.query(prompt.question, prompt.img_url, context)
     #agregar la pregunta que se realiza al sistema rag
-    return {"response": response, "question": rag_system.}
+    return {"response": response, "question": rag_system}
 
 @app.post("/querynoimage/")
 async def query_rag(prompt: Question):
